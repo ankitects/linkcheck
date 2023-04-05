184b2ca50ed39ca43da13f0b830a463861adb9ca
RumovZ <gp5glkw78@relay.firefox.com>
2023-04-05 20:03:07 +0200

    Cache all fragments from parsed page

--- src/validation/web.rs
+++ src/validation/web.rs
@@ -1,11 +1,9 @@
 use crate::validation::{CacheEntry, Context, Reason};
-use html5ever::parse_document;
-use html5ever::tendril::TendrilSink;
+use html5ever::{parse_document, tendril::TendrilSink};
 use http::HeaderMap;
-use markup5ever_rcdom::{Handle, NodeData, RcDom};
+use markup5ever_rcdom::{NodeData, RcDom};
 use reqwest::{Client, Response, Url};
-use std::borrow::Borrow;
-use std::time::SystemTime;
+use std::{borrow::Borrow, time::SystemTime};
 
 /// Send a GET request to a particular endpoint.
 pub async fn get(
@@ -49,51 +47,78 @@ where
         return Ok(());
     }
 
-    let result = if let Some(fragment) = url.fragment() {
-        log::debug!("Checking \"{}\" contains \"{}\"", url, fragment);
-        let response =
-            get(ctx.client(), url.clone(), ctx.url_specific_headers(url))
-                .await?;
-        if element_with_id_exists(response.text().await?.as_bytes(), fragment) {
-            Ok(())
-        } else {
-            Err(Reason::Dom)
-        }
-    } else {
-        head(ctx.client(), url.clone(), ctx.url_specific_headers(url))
-            .await
-            .map_err(Reason::from)
-    };
-
-    let entry = CacheEntry::new(SystemTime::now(), result.is_ok());
-    update_cache(url, ctx, entry);
-
-    result
+    match url.fragment() {
+        Some(fragment) => {
+            log::debug!("Checking \"{}\" contains \"{}\"", url, fragment);
+            check_fragment_url(url, fragment, ctx).await
+        },
+        None => {
+            let result =
+                head(ctx.client(), url.clone(), ctx.url_specific_headers(url))
+                    .await
+                    .map_err(Reason::from);
+            update_cache(url, ctx, result.is_ok());
+            result
+        },
+    }
 }
 
-fn element_with_id_exists(mut html: &[u8], id: &str) -> bool {
+async fn check_fragment_url(
+    url: &Url,
+    fragment: &str,
+    ctx: &(impl Context + ?Sized),
+) -> Result<(), Reason> {
+    let response =
+        get(ctx.client(), url.clone(), ctx.url_specific_headers(url)).await?;
+    cache_url_fragment(ctx, url, None);
+
+    let mut found = false;
+    walk_element_ids(response.text().await?.as_bytes(), |id: &str| {
+        cache_url_fragment(ctx, url, Some(id));
+        found |= id == fragment;
+        // if caching, process all ids
+        found && ctx.cache().is_none()
+    });
+
+    if found {
+        Ok(())
+    } else {
+        Err(Reason::Dom)
+    }
+}
+
+fn cache_url_fragment(
+    ctx: &(impl Context + ?Sized),
+    url: &Url,
+    fragment: Option<&str>,
+) {
+    if let Some(mut cache) = ctx.cache() {
+        let mut fragment_url = url.clone();
+        fragment_url.set_fragment(fragment);
+        let entry = CacheEntry::new(SystemTime::now(), true);
+        cache.insert(fragment_url, entry);
+    }
+}
+
+/// Walk element ids until `processor` returns true.
+fn walk_element_ids(mut html: &[u8], mut processor: impl FnMut(&str) -> bool) {
     let dom = parse_document(RcDom::default(), Default::default())
         .from_utf8()
         .read_from(&mut html)
         .unwrap();
-    node_has_element_with_id(&dom.document, id)
-}
-
-fn node_has_element_with_id(node: &Handle, id: &str) -> bool {
-    let node_data = node.data.borrow();
-    if let NodeData::Element { ref attrs, .. } = *node_data {
-        if attrs
-            .borrow()
-            .iter()
-            .any(|a| a.name.local.as_ref() == "id" && a.value.as_ref() == id)
-        {
-            return true;
+    let mut stack = vec![dom.document];
+    while let Some(node) = stack.pop() {
+        if let NodeData::Element { ref attrs, .. } = *node.data.borrow() {
+            for attr in attrs.borrow().iter() {
+                if attr.name.local.as_ref() == "id"
+                    && processor(attr.value.as_ref())
+                {
+                    return;
+                }
+            }
         }
+        stack.extend(std::mem::take(&mut *node.children.borrow_mut()));
     }
-    node.children
-        .borrow()
-        .iter()
-        .any(|child| node_has_element_with_id(child, id))
 }
 
 fn already_valid<C>(url: &Url, ctx: &C) -> bool
@@ -107,11 +132,12 @@ where
     false
 }
 
-fn update_cache<C>(url: &Url, ctx: &C, entry: CacheEntry)
+fn update_cache<C>(url: &Url, ctx: &C, valid: bool)
 where
     C: Context + ?Sized,
 {
     if let Some(mut cache) = ctx.cache() {
+        let entry = CacheEntry::new(SystemTime::now(), valid);
         cache.insert(url.clone(), entry);
     }
 }
